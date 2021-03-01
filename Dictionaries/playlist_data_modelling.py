ce28016862d9597db5168660de74efc027cd7c67
prithviBytes <prithvisuvarna56@gmail.com>
2021-03-01 23:57:19 +0530

    Add Playlist Data Model

---
+++ Dictionaries/playlist_data_modelling.py
@@ -0,0 +1,18 @@
+playlist = {
+    "title": "Romantic Songs",
+    "author": "Prithvi Suvarna",
+    "songs": [
+        {"title": "Kun Faya","artists": ["AR. Rahman","Mohit Chauhan"],"duration": 3.5},
+        {"title": "Barso","artists": ["Ritviz"],"duration": 2.5},
+        {"title": "Kun Faya","artists": ["AR. Rahman","Mohit Chauhan"],"duration": 3.5},
+        {"title": "Barso","artists": ["Ritviz"],"duration": 2.5},
+        {"title": "Kun Faya","artists": ["AR. Rahman","Mohit Chauhan"],"duration": 3.5}
+    ]
+}
+
+print(f"Playlist title: {playlist.get('title')}")
+print(f"Author: {playlist['author']}")
+
+for song in playlist["songs"]:
+    artists = " ".join(artist for artist in song['artists'])
+    print(f"Song Name: {song['title']} Artists: {artists} Durations: {song['duration']}")
