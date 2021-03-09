602389e29b574fc91a78664185d09b4296481287
prithviBytes <prithvisuvarna56@gmail.com>
2021-03-10 01:21:43 +0530

    Add name variable files

--- Modules/testing_name_variable.py
+++ Modules/testing_name_variable.py
@@ -1,3 +1,6 @@
 from name_variable import say_hi
 
-say_hi() # prints the file name of the function
+say_hi()  # prints the file name of the function
+
+# This is executed twice, once when the file is imported python executes the
+# enitre file.
