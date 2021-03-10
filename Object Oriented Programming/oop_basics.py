7b498db99bfbc0e57c968515525a7086fa3ff3ee
prithviBytes <prithvisuvarna56@gmail.com>
2021-03-10 18:33:10 +0530

    Add OOP basic file

---
+++ Object Oriented Programming/oop_basics.py
@@ -0,0 +1,26 @@
+# What is OOP?
+# Object oriented programming is a style of programming in which
+# we try to model some process or thing in the real world using
+# classes and objects.
+
+# What is a class?
+# Its is a blueprint for objects. Classes can contain methods(functions)
+# and attributes.
+
+# What is an instance?
+# Objects that are constructed using the class blueprint, that contains the
+# class methods and properties.
+
+# Why OOP?
+# With OOP the goal is to encapsulate your code into logical, hierarchical,
+# groupings using class so that you can reason about your code in a higher level.
+# PS: Its not necessary.
+
+# What is Encapsulation?
+# Grouping of public and private attributes and methods making abstraction possible.
+# Example: DECK OF CARD: The cards can be private. But we might need the length of the cards
+# so for that we will create a public method which can then be access outside.
+
+# What is Abstraction?
+# Abstraction is exposing only relevant data in the class interface, hiding private attributes
+# and methods (inner workings) from the user.
