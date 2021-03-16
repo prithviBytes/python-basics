97455ec2df2f8ff427fa9473ea0f3eb2912c879b
prithviBytes <prithvisuvarna56@gmail.com>
2021-03-16 20:38:09 +0530

    Add MRO example

---
+++ OOP Advance/MRO example.py
@@ -0,0 +1,16 @@
+class Mother:
+    def __init__(self):
+        self.eye_color = "brown"
+        self.hair_color = "dark brown"
+        self.hair_type = "curly"
+        
+class Father:
+    def __init__(self):
+        self.eye_color = "blue"
+        self.hair_color = "blonde"
+        self.hair_type = "straight"
+
+class Child(Mother,Father):
+    
+    def __init__(self):
+        super().__init__()
