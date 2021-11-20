b77fa082da43f1d593e736620c0809b466a48fee
prithviBytes <prithvisuvarna56@gmail.com>
2021-11-20 19:11:25 +0530

    add: special method train

---
+++ OOP Advance/special_methods_train.py
@@ -0,0 +1,10 @@
+class Train:
+    
+    def __init__(self, num_cars):
+        self.num_cars = num_cars
+    
+    def __repr__(self):
+        return "{0} car train".format(self.num_cars)
+        
+    def __len__(self):
+        return self.num_cars
